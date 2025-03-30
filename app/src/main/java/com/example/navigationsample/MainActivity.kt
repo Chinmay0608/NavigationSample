88bf9e1e90a10c2125b552923b337580d3bbd49c
Chinmay Maheshwari <chinmay8064@gmail.com>
2025-03-30 09:58:47 +0530

    -m "This is my second commit"

---
+++ app/src/main/java/com/example/navigationsample/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.navigationsample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigationsample.ui.theme.NavigationSampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavigationSampleTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "firstScreen") {
+        composable(route = "firstScreen") { FirstScreen() {name,age ->
+            navController.navigate("secondScreen/$name/$age")
+        } }
+        composable(route = "secondScreen/{name}{age}") {
+            val name =it.arguments?.getString("name")?:"no name"
+            var age =it.arguments?.getInt("age")?:0
+            SecondScreen(name,age) {name,age ->
+                navController.navigate("thirdScreen/$name/$age")
+            } }
+        composable(route = "thirdScreen") {
+            ThirdScreen() {
+                navController.navigate("firstScreen")
+            } }
+    }
+}
---
+++ app/src/main/java/com/example/navigationsample/ThirdScreen.kt
@@ -0,0 +1,32 @@
+package com.example.navigationsample
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ThirdScreen(navigateToFirstScreen: () -> Unit){
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("This is the Third Screen", fontSize = 24.sp)
+        Text("Last Screen", fontSize = 24.sp)
+
+        Button(onClick = {
+            navigateToFirstScreen()
+        } ) {
+            Text("Go to First Screen")
+        }
+    }
+}
